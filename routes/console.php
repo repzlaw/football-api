0ee2c1d88cb5cfb4707552d9ae0e1e08aded33b0
repzlaw <ibitoyedavid@gmail.com>
2024-07-18 21:17:01 +0100

    scheduled console commands

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,11 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\UpdateFixtures;
+use Illuminate\Support\Facades\Schedule;
+use App\Console\Commands\UpdateClubsVenues;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Schedule::command(UpdateClubsVenues::class)->weekly();
+
+Schedule::command(UpdateFixtures::class)->everyTenMinutes();
+
+Schedule::command(UpdateFixtures::class, ['--type=all'])->daily();
